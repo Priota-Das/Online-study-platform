946c2b546caf69e194642f83b4f75fbfbe92c9ce
Priota-Das <priotadas.7@gmail.com>
2025-05-02 14:06:10 +0600

    Update userValidate.middleware.js

--- backend/src/middlewares/userValidate.middleware.js
+++ backend/src/middlewares/userValidate.middleware.js
@@ -1,8 +1,7 @@
 import {validateUserSchema} from "../lib/validateUserSchema.js";
 
 export const validate =(req,res,next)=>{
-    const parseResult = validateUserSche
-    ma.safeParse(req.body);
+    const parseResult = validateUserSchema.safeParse(req.body);
     if(!parseResult.success){
         return res.status(400).json({
             errors:parseResult.error.format()
